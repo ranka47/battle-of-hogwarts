58a38250e0459b43ae9ee0a623e866b1a6800fb8
Sumeet Ranka <sumeet.ranka47@localhost.localdomain>
2014-09-03 22:35:59 +0530

    Addition to previous commit

--- src/commands/connection_screen.py
+++ src/commands/connection_screen.py
@@ -8,7 +8,7 @@ from src.utils import utils
 
 DEFAULT_SCREEN = \
 """{b=============================================================={n
- Welcome to {g%s{n, version %s!
+ Welcome to {g%s{n!
 
  If you have an existing account, connect to it by typing:
       {wconnect <username> <password>{n
@@ -18,4 +18,4 @@ DEFAULT_SCREEN = \
  If you have spaces in your username, enclose it in quotes.
  Enter {whelp{n for more info. {wlook{n will re-show this screen.
 {b=============================================================={n""" \
-% (settings.SERVERNAME, utils.get_evennia_version())
+% (settings.SERVERNAME)
